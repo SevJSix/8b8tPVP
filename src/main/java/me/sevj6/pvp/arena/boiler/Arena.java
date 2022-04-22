e93b5f59ab96500da59eca7b57c9878dc04eba2b
Nate Steven Legault <67813185+254nm@users.noreply.github.com>
2022-04-21 23:21:07 -0400

    Fix broken arena stuff

--- src/main/java/me/sevj6/pvp/arena/boiler/Arena.java
+++ src/main/java/me/sevj6/pvp/arena/boiler/Arena.java
@@ -8,7 +8,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
+
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
@@ -19,7 +19,6 @@ import java.util.stream.Collectors;
 public class Arena extends AbstractArena {
 
     protected List<Material> invalidMaterials = Arrays.asList(Material.AIR, Material.BEDROCK);
-    private final List<Location> locations = getAllLocations(getFirstPosition(), getSecondPosition());
 
     public Arena(String name, World world, int x1, int y1, int z1, int x2, int y2, int z2) {
         super(name, world, x1, y1, z1, x2, y2, z2);
@@ -30,7 +29,7 @@ public class Arena extends AbstractArena {
     }
 
     public void loadArenaChunks() {
-        locations.forEach(location -> {
+        getAllLocations(getFirstPosition(), getSecondPosition()).forEach(location -> {
             if (!location.getChunk().isLoaded()) location.getChunk().load();
         });
     }
@@ -38,7 +37,7 @@ public class Arena extends AbstractArena {
     @Override
     public void clear() {
         long start = System.currentTimeMillis();
-        findAllBlocks(locations).forEach(block -> {
+        findAllBlocks(getAllLocations(getFirstPosition(), getSecondPosition())).forEach(block -> {
             block.setType(Material.AIR);
         });
         long finish = (System.currentTimeMillis() - start);
@@ -55,7 +54,7 @@ public class Arena extends AbstractArena {
     }
 
     @Override
-    public List<Location> getAllLocations(BlockPosition pos1, BlockPosition pos2) {
+    public  List<Location> getAllLocations(BlockPosition pos1, BlockPosition pos2) {
         if (getWorld() == null) throw new NullPointerException("World in arena '" + getName() + "' is null!");
         List<Location> locations = new ArrayList<>();
         for (int x = getMinX(); x <= getMaxX(); x++) {
@@ -72,7 +71,7 @@ public class Arena extends AbstractArena {
 
     @Override
     public List<Block> findAllBlocks(List<Location> locations) {
-        return locations.stream().map(Location::getBlock).collect(Collectors.toList());
+        return getAllLocations(getFirstPosition(), getSecondPosition()).stream().map(Location::getBlock).collect(Collectors.toList());
     }
 
     @Override
